416be4e3d3f967cf457225031f0e7fa3f1a22c1b
Pham Duc Hoang <hoangggp@gmail.com>
2025-02-27 09:05:57 +0700

    update

--- loader/ae_loader.py
+++ loader/ae_loader.py
@@ -14,9 +14,11 @@ class Loader4AE(torch.utils.data.Dataset):
         if env.args.dataset == 'baby':
             self.n_user, self.m_item = 19445, 7050
         elif env.args.dataset == 'clothing':
-            self.n_user, self.m_item = 39387, 23033
+            # self.n_user, self.m_item = 39387, 23033 
+            self.n_user, self.m_item = 39387, 22498
         elif env.args.dataset == 'sports':
-            self.n_user, self.m_item = 35598, 18357
+            # self.n_user, self.m_item = 35598, 18357
+            self.n_user, self.m_item = 35598, 18287
         
         train_file = os.path.join(self.env.DATA_PATH, 'train.txt')
         val_file = os.path.join(self.env.DATA_PATH, 'val.txt')
